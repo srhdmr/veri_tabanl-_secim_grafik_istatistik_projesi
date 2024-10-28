1aa480aeb1e108a0c9a044c93a41536abc2391eb
Serhat <serhattdemirr2005@gmail.com>
2024-10-28 12:37:56 +0300

    Proje dosyası ekle.

---
+++ veri_tabanlı_secim_grafik_istatistik_projesi/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace veri_tabanlı_secim_grafik_istatistik_projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtİlçeAd = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.btnOyGiriş = new System.Windows.Forms.Button();
+            this.btnGrafikler = new System.Windows.Forms.Button();
+            this.btnÇıkış = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(-4, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İLÇE ADI: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(9, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A PARTİ : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(9, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "B PARTİ : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(9, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "C PARTİ : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label5.Location = new System.Drawing.Point(9, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 38);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "D PARTİ : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label6.Location = new System.Drawing.Point(9, 436);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 38);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "E PARTİ : ";
+            // 
+            // txtİlçeAd
+            // 
+            this.txtİlçeAd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.txtİlçeAd.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtİlçeAd.Location = new System.Drawing.Point(146, 24);
+            this.txtİlçeAd.Name = "txtİlçeAd";
+            this.txtİlçeAd.Size = new System.Drawing.Size(386, 42);
+            this.txtİlçeAd.TabIndex = 6;
+            // 
+            // txtA
+            // 
+            this.txtA.BackColor = System.Drawing.Color.Coral;
+            this.txtA.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtA.Location = new System.Drawing.Point(160, 162);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(363, 42);
+            this.txtA.TabIndex = 7;
+            // 
+            // txtB
+            // 
+            this.txtB.BackColor = System.Drawing.Color.Coral;
+            this.txtB.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtB.Location = new System.Drawing.Point(160, 227);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(363, 42);
+            this.txtB.TabIndex = 8;
+            // 
+            // txtC
+            // 
+            this.txtC.BackColor = System.Drawing.Color.Coral;
+            this.txtC.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtC.Location = new System.Drawing.Point(160, 294);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(363, 42);
+            this.txtC.TabIndex = 9;
+            // 
+            // txtD
+            // 
+            this.txtD.BackColor = System.Drawing.Color.Coral;
+            this.txtD.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtD.Location = new System.Drawing.Point(160, 356);
+            this.txtD.Name = "txtD";
+            this.txtD.Size = new System.Drawing.Size(363, 42);
+            this.txtD.TabIndex = 10;
+            // 
+            // txtE
+            // 
+            this.txtE.BackColor = System.Drawing.Color.Coral;
+            this.txtE.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtE.Location = new System.Drawing.Point(160, 436);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(363, 42);
+            this.txtE.TabIndex = 11;
+            // 
+            // btnOyGiriş
+            // 
+            this.btnOyGiriş.BackColor = System.Drawing.Color.DimGray;
+            this.btnOyGiriş.Font = new System.Drawing.Font("Italic Outline Art", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnOyGiriş.ForeColor = System.Drawing.Color.Red;
+            this.btnOyGiriş.Location = new System.Drawing.Point(568, 50);
+            this.btnOyGiriş.Name = "btnOyGiriş";
+            this.btnOyGiriş.Size = new System.Drawing.Size(220, 94);
+            this.btnOyGiriş.TabIndex = 12;
+            this.btnOyGiriş.Text = "OY GİRİŞİ YAP";
+            this.btnOyGiriş.UseVisualStyleBackColor = false;
+            this.btnOyGiriş.Click += new System.EventHandler(this.btnOyGiriş_Click);
+            // 
+            // btnGrafikler
+            // 
+            this.btnGrafikler.BackColor = System.Drawing.Color.DimGray;
+            this.btnGrafikler.Font = new System.Drawing.Font("Italic Outline Art", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnGrafikler.ForeColor = System.Drawing.Color.Red;
+            this.btnGrafikler.Location = new System.Drawing.Point(568, 171);
+            this.btnGrafikler.Name = "btnGrafikler";
+            this.btnGrafikler.Size = new System.Drawing.Size(220, 98);
+            this.btnGrafikler.TabIndex = 13;
+            this.btnGrafikler.Text = "GRAFİKLER";
+            this.btnGrafikler.UseVisualStyleBackColor = false;
+            this.btnGrafikler.Click += new System.EventHandler(this.btnGrafikler_Click);
+            // 
+            // btnÇıkış
+            // 
+            this.btnÇıkış.BackColor = System.Drawing.Color.DimGray;
+            this.btnÇıkış.Font = new System.Drawing.Font("Italic Outline Art", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnÇıkış.ForeColor = System.Drawing.Color.Red;
+            this.btnÇıkış.Location = new System.Drawing.Point(568, 316);
+            this.btnÇıkış.Name = "btnÇıkış";
+            this.btnÇıkış.Size = new System.Drawing.Size(220, 98);
+            this.btnÇıkış.TabIndex = 14;
+            this.btnÇıkış.Text = "ÇIKIŞ YAP";
+            this.btnÇıkış.UseVisualStyleBackColor = false;
+            this.btnÇıkış.Click += new System.EventHandler(this.btnÇıkış_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(800, 504);
+            this.Controls.Add(this.btnÇıkış);
+            this.Controls.Add(this.btnGrafikler);
+            this.Controls.Add(this.btnOyGiriş);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.txtD);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtİlçeAd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VTGS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtİlçeAd;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtD;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.Button btnOyGiriş;
+        private System.Windows.Forms.Button btnGrafikler;
+        private System.Windows.Forms.Button btnÇıkış;
+    }
+}
+
---
+++ veri_tabanlı_secim_grafik_istatistik_projesi/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace veri_tabanlı_secim_grafik_istatistik_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=SerhatDemir\SQLEXPRESS;Initial Catalog=DBSECIMPROJE;Integrated Security=True;");
+        private void btnOyGiriş_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TBLILCE (ILCEAD,APARTI,BPARTI,CPARTI,DPARTI,EPARTI) values(@P1,@P2,@P3,@P4,@P5,@P6)",baglanti);
+            komut.Parameters.AddWithValue("@P1", txtİlçeAd.Text);
+            komut.Parameters.AddWithValue("@P2", txtA.Text);
+            komut.Parameters.AddWithValue("@P3", txtB.Text);
+            komut.Parameters.AddWithValue("@P4", txtC.Text);
+            komut.Parameters.AddWithValue("@P5", txtD.Text);
+            komut.Parameters.AddWithValue("@P6", txtE.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("OY GİRİŞİ YAPILDI");
+        }
+
+        private void btnGrafikler_Click(object sender, EventArgs e)
+        {
+            FrmGrafikler fr = new FrmGrafikler();
+            fr.Show();
+        }
+
+        private void btnÇıkış_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    }
